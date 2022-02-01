4520abfdc951cf9a74d92d3a0179cbcda13f787f
dani <d_krasimirov@yahoo.com>
2022-02-01 20:32:52 +0100

    -

--- output.tf
+++ output.tf
@@ -14,6 +14,6 @@
 
 output "application_access" {
     value = [for x in module.container[*]: x]
-    description = "The name and the socket fo each app"
+    description = "The name and the socket fo each applicat"
 }
 
